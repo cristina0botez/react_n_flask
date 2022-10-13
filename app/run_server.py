92f8c494da61acbaec15da13abb7fbe3debf8f83
Cristina Botez <cristina0botez@gmail.com>
2022-10-13 18:41:32 +0300

    Flask project

---
+++ app/run_server.py
@@ -0,0 +1,5 @@
+from app.http.api.endpoints import app
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+
+setup(name='react_n_flask',
+      version='0.1',
+      description='React and Flask',
+      author='cris',
+      packages=['app', 'app.http', 'app.http.api', 'app.kudo', 'app.repository'])
